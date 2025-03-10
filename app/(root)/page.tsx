5d8363e621c6aae135f9fab772ffe6e651fa3048
sampreeth <sampreethreddy13579@gmail.com>
2025-03-10 13:41:23 +0530

    Add authentication, components, and theme configuration

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <div className="flex justify-between items-center mb-8">
+       
+      </div>
+    </main>
+  );
+}
